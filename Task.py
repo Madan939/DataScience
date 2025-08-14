1b169da7b54a838df66a1594b70238f32747f5b7
Madan Golay Tamang <madantamang939@gmail.com>
2025-08-14 15:02:37 +0545

    Task 3 added

--- Task.py
+++ Task.py
@@ -1,5 +1,18 @@
 # Task 1. WAP to print your name
 print("Madan Golay Tamang")
-# Task 2. (Input a gavourite player and print it's name)
+# Task 2. Input a Favourite player and print it's name
 name=input("Enter your favourite player name: ")
 print(name)
+# Task 3. Input for every data type and print it
+
+num=int(input("Enter any integer: "))#integer
+name=input("Enter any string: ")# string
+float_num=float(input("Enter any floating number: ")) # string
+
+#print type 
+print(type(num))
+print(type(name))
+print(type(float_num))
+
+#print all
+print(f"Integer= {num}, String= {name}, float= {float_num}")
---
+++ basicPython.py
@@ -0,0 +1,12 @@
+# typecasting
+print(int("56"))
+print(float("4.55"))
+print(int(2.22))
+
+#data type
+print(type(123))
+print(type("Hello"))
+print(type(2.44))
+print(type(False))
+name=["abc","123"]
+print(type(name))
