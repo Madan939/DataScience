fa6be7d6c017ad396e457ab94eadf4b765485b11
Madan Golay Tamang <madantamang939@gmail.com>
2025-08-18 09:05:46 +0545

    String task completed

--- Task.py
+++ Task.py
@@ -18,47 +18,72 @@
 #print(f"Integer= {num}, String= {name}, float= {float_num}")
 
 # Task 4. Ask input of two numbers from the user and perform operations on those numbers or values
-num1=int(input("Enter first number: "))
-num2=int(input("Enter second number: "))
+# num1=int(input("Enter first number: "))
+# num2=int(input("Enter second number: "))
 # Operations
-print(num1+num2)
-print(num1==num2)
-print(num1%num2)
-print(num1>num2)
-print(num1**3)
-print(num1*num2)
-print(num1/num2)
-print(num1//num2)
-print(num1==5 and num2!=3)
-print(num1==5 or num2<4)
-num2+=num1
-print(num2)
+# print(num1+num2)
+# print(num1==num2)
+# print(num1%num2)
+# print(num1>num2)
+# print(num1**3)
+# print(num1*num2)
+# print(num1/num2)
+# print(num1//num2)
+# print(num1==5 and num2!=3)
+# print(num1==5 or num2<4)
+# num2+=num1
+# print(num2)
 
-# Task 5. Ask user a sstring and print if b is in that string or not. Do use in and not in
+# Task 5. Ask user a string and print if b is in that string or not. Do use in and not in
 
-name=input("Enter your name: ")
-if "b" in name:
-    print("b is present")
-elif "b" not in name:
-    print("b is absent by not in method")
-else:
-    print("b is absent")
+# name=input("Enter your name: ")
+# if "b" in name:
+#     print("b is present")
+# elif "b" not in name:
+#     print("b is absent by not in method")
+# else:
+#     print("b is absent")
 
 # Simple calculator
-num_1=float(input("Enter first number: "))
-num_2=float(input("Enter second number: "))
-operator=input("Enter operators: ")
-if(operator=="+" and operator=="-" and operator=="*" and operator=="/" and operator=="%"):
-    match operator:
-        case "+":
-            print(num_1+num_2)
-        case "-":
-            print(num_1-num_2)
-        case "*":
-            print(num_1*num_2)
-        case "/":
-            print(num_1/num_2)
-        case "%":
-            print(num_1%num_2)
-else:
-    print("operators didnot match:")
+# num_1=float(input("Enter first number: "))
+# num_2=float(input("Enter second number: "))
+# operator=input("Enter operator: ")
+# if(operator=="+" and operator=="-" and operator=="*" and operator=="/" and operator=="%"):
+#     match operator:
+#         case "+":
+#             print(num_1+num_2)
+#         case "-":
+#             print(num_1-num_2)
+#         case "*":
+#             print(num_1*num_2)
+#         case "/":
+#             print(num_1/num_2)
+#         case "%":
+#             print(num_1%num_2)
+#         case _:
+#             print("operator not found")
+            
+# else:
+#     print("operators didnot match:")
+
+#Task: 6. Implement all the indexing and slicing in string 
+name="mindrisers institute"
+print(name)
+print(name.capitalize())
+print(name.upper())
+print(name.strip())
+print(name.replace("m","M"))
+print(name.split(" "))
+
+print(name[0])
+
+#positive index
+print(name[0:4])
+print(name[:5])
+print(name[4:])
+
+#negative index
+print(name[-1])
+print(name[:-3])
+print(name[-2:])
+print(name[-4:-1])
--- basicPython.py
+++ basicPython.py
@@ -13,4 +13,28 @@
 
 #mutable and non-mutable
 #Those data type that can change it's value programatically is mutable,and by creating new objecta we can change data is non-mutable
-
+#List = list is a data type in python that can store multiple values of different data type.
+# even we can store another list inside list
+# we use [] to define a list
+#example
+fruits=["Mango","Orange","Guava","Pineapple","Lemon","Water-melon"]
+print(fruits)
+fruits.append("Papaya")
+print(fruits)
+print(fruits.count("Mango"))
+print(fruits.index("Guava"))
+fruits.remove("Guava")
+fruits.pop(1)
+print(fruits)
+#index
+#positive index
+print(fruits[2])#get value from index
+print(fruits[0:2])#get value from start to end
+print(fruits[:3])#get value from end
+print(fruits[2:])#get value from start
+print(fruits[::2])# index with steps
+# negative index
+print(fruits[-1])# get value with negative index
+print(fruits[-3:-1])
+print(fruits[:-2])
+print(fruits[-2:])
