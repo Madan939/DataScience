ddc139c9b67cd0eaa65d1f8fc8a420c36af62dd5
Madan Golay Tamang <madantamang939@gmail.com>
2025-08-19 15:03:49 +0545

    Task updated

--- Task.py
+++ Task.py
@@ -44,46 +44,107 @@
 # else:
 #     print("b is absent")
 
-# Simple calculator
+
+
+#Task: 6. Implement all the indexing and slicing in string 
+# name="mindrisers institute"
+# print(name)
+# print(name.capitalize())
+# print(name.upper())
+# print(name.strip())
+# print(name.replace("m","M"))
+# print(name.split(" "))
+
+# print(name[0])
+
+#positive index
+# print(name[0:4])
+# print(name[:5])
+# print(name[4:])
+
+#negative index
+# print(name[-1])
+# print(name[:-3])
+# print(name[-2:])
+# print(name[-4:-1])
+
+# Task.7: Get input from user and check if user name is in the list or not
+# name=["Ram","Shyam","Hari","Madan"]
+# password=["sksjbsuuwef","jskdncjbsc","sdjncduu","8ehduhedd"]
+# user=input("Enter your name:")
+# if user in name:
+#     print(f"{user} is in the list")
+# else:
+#     print(f"{user} not found")
+
+# Task.8: Create a dictionary of usernames and passwords, extract all the usernames from the dictionary and input username from the user and check if the username is present in the extracted list of usernames
+# credentials={
+#     "Ram":"ramabc",
+#     "Shyam":"shyamabc",
+#     "Hari":"hariabc"
+# }
+#print all username
+# for x in credentials.keys():
+#     print (x)
+# check if username is present or not
+# name=input("Enter the username:")
+# if name in credentials.keys():
+#     print(f"{name} is in the list")
+# else:
+#     print(f"{name} is not in the list")
+#Simple calculator
 # num_1=float(input("Enter first number: "))
 # num_2=float(input("Enter second number: "))
 # operator=input("Enter operator: ")
-# if(operator=="+" and operator=="-" and operator=="*" and operator=="/" and operator=="%"):
-#     match operator:
-#         case "+":
-#             print(num_1+num_2)
-#         case "-":
-#             print(num_1-num_2)
-#         case "*":
-#             print(num_1*num_2)
-#         case "/":
-#             print(num_1/num_2)
-#         case "%":
-#             print(num_1%num_2)
-#         case _:
-#             print("operator not found")
-            
+
+#using if,elif,else
+# if operator=="+":
+#     print("Addition= "+num_1+num_2)
+# elif operator=="-":
+#     print("Subtraction= "+num_1-num_2)
+# elif operator=="*":
+#     print("Multiplication= "+num_1*num_2)
+# elif operator=="/":
+#     print("Division= "+num_1/num_2)
+# elif operator=="%":
+#     print("Modulus= "+num_1%num_2)
 # else:
 #     print("operators didnot match:")
+    
+#using match
+# match operator:
+#     case "+":
+#         print("Addition= "+num_1+num_2)
+#     case "-":
+#         print("Subtraction= "+num_1-num_2)
+#     case "*":
+#         print("Multiplication= "+num_1*num_2)
+#     case "/":
+#         print("Division= "+num_1/num_2)
+#     case "%":
+#         print("Modulus= "+num_1%num_2)
+#     case _:
+#         print("operator not found")
+        
+# Task : Make login and register system
+credentials={
+    "Ram":"ram123",
+    "Shyam":"shyam123"
+}
+user_input=int(input("Enter: 1 for login, 2. for register"))
 
-#Task: 6. Implement all the indexing and slicing in string 
-name="mindrisers institute"
-print(name)
-print(name.capitalize())
-print(name.upper())
-print(name.strip())
-print(name.replace("m","M"))
-print(name.split(" "))
-
-print(name[0])
-
-#positive index
-print(name[0:4])
-print(name[:5])
-print(name[4:])
-
-#negative index
-print(name[-1])
-print(name[:-3])
-print(name[-2:])
-print(name[-4:-1])
+match user_input:
+    case 1:
+        username=input("Enter user name:")
+        password=input("Enter password: ")
+        if username in credentials and credentials.get(username)==password:
+            print("Login successfully")
+        else:
+            print("login failed")
+    case 2:
+        username=input("Enter user name: ")
+        password=input("Enter password: ")
+        credentials.update({username:password})
+        print("registered successfully")
+    case _:
+        print("input didnot match")
--- basicPython.py
+++ basicPython.py
@@ -17,24 +17,95 @@
 # even we can store another list inside list
 # we use [] to define a list
 #example
-fruits=["Mango","Orange","Guava","Pineapple","Lemon","Water-melon"]
-print(fruits)
-fruits.append("Papaya")
-print(fruits)
-print(fruits.count("Mango"))
-print(fruits.index("Guava"))
-fruits.remove("Guava")
-fruits.pop(1)
-print(fruits)
+#fruits=["Mango","Orange","Guava","Pineapple","Lemon","Water-melon"]
+#print(fruits)
+#fruits.append("Papaya")
+#print(fruits)
+#print(fruits.count("Mango"))
+#print(fruits.index("Guava"))
+#fruits.remove("Guava")
+#fruits.pop(1)
+#print(fruits)
 #index
 #positive index
-print(fruits[2])#get value from index
-print(fruits[0:2])#get value from start to end
-print(fruits[:3])#get value from end
-print(fruits[2:])#get value from start
-print(fruits[::2])# index with steps
+#print(fruits[2])#get value from index
+#print(fruits[0:2])#get value from start to end
+#print(fruits[:3])#get value from end
+#print(fruits[2:])#get value from start
+#print(fruits[::2])# index with steps
 # negative index
-print(fruits[-1])# get value with negative index
-print(fruits[-3:-1])
-print(fruits[:-2])
-print(fruits[-2:])
+#print(fruits[-1])# get value with negative index
+# print(fruits[-3:-1])
+# print(fruits[:-2])
+# print(fruits[-2:])
+ # Tuple: Tuple is a immutable data type that wraps the element in ()
+# num=(1,2,3,4,5,6)
+# print(num.count(4))
+# print(num[3])
+# name=["a","b"]
+# tup=tuple(name)
+# print(tup)
+# print(type(tup))
+
+# Set
+# name={"Ram","Shyam","Hari","Geeta"}
+# print(name)
+# name.add("Sita")
+# print(name)
+# name.remove("Ram")
+# print(name)
+# classes={1,2,4}
+# unions=name | classes
+# print(unions)
+# name.update(unions)
+# print(name)
+# unions.clear()
+# print(unions)
+# num1={1,2,3,4,5,6}
+# num2={3,4,6,7,8,9}
+# print(num1.difference(num2))
+# print(num1.intersection(num2))
+# num1.add(8)
+# num1.remove(4)
+# num1.pop()
+# print(num1)
+# Dictionary
+# car={
+#     "Founder":"Abc",
+#     "Industry":"Automobiles",
+#     "Brand":"Toyata",
+#     "Date":2055
+# }
+# print(car)
+#access the items
+# print(car["date"])
+#change the value
+# car["brand"]="Tesla"
+# print(car)
+#after adding
+# car["color"]="red"
+# print(car)
+#remove item
+# car.pop("date")
+# print(car)
+#print keys only
+# for x in car.keys():
+#     print(x)
+#print values only
+# for y in car.values():
+#     print(y)
+#using get methods
+# print(car.get("color"))
+#using update methods
+# car.update({"color":"White"})
+# print(car)
+# use copy and clear the dictionary
+# newCar=car.copy()
+# car.clear()
+# print(car)
+# print(newCar)
+#use popitem method
+# newCar.popitem()
+# print(newCar)
+# for x,y in newCar.items():
+#     print(f"{x} = {y}")
