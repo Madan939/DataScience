6f05a5537be6d79aac4d935e713a7128f56b7e5f
Madan Golay Tamang <madantamang939@gmail.com>
2025-08-21 17:47:58 +0545

    Login & register system,Calculator using methods task completed

---
+++ methods.py
@@ -0,0 +1,27 @@
+# function
+# def myClass():
+#     print("This is function")
+# myClass() 
+
+
+
+# Table creation using methods and loops
+
+# def table(num):
+#     i=0
+#     while i<=9:
+#         mul=i+1
+#         print(f"{num} * {mul} = {num*mul}")
+#         i+=1
+# while True:
+#     number_input=int(input("Enter:\n1. For table\n2. For Exit\n"))
+#     if number_input==2:
+#         print("Existing...")
+#         break
+#     num=int(input("Enter the number that you want to create table:"))
+#     match number_input:
+#         case 1:
+#             table(num)
+#         case _:
+#             print("invalid input...")
+    
